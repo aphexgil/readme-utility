8c7b9e2801bc6b8662678940a097ad183f4747f6
aphexgil <gil.young.1994@gmail.com>
2022-12-22 09:33:17 -0800

    Initial commit. Uploading starter code from class repo.

---
+++ index.js
@@ -0,0 +1,23 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    'What is the title of your project?',
+    'Provide a short description of your project.',
+    'How does a user install the project?',
+    'Provide instructions and examples for use.',
+    'List your collaborators, if any, with links to their GitHub profiles.',
+    
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
